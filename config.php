24a30f5f60b415614a7a51f3dda594dda5ae6322
Matus <matusjas.work@gmail.com>
2017-09-12 16:27:53 +0200

    Little changes

--- config.php
+++ config.php
@@ -2,11 +2,14 @@
 
 	use Matmaus\Windower;
 
-	//expect "Windower" file to be exactly in root file
+	//expect "Windower" folder to be exactly in root file
 	require_once 'Windower.php';
 	$windower_array = [];
 
 	//      SETTINGS        //
+	// IF YOU HAVE ANY TROUBLES, LOOK AT EXAMPLES
+
+	//setup database if you need it and you did not configured it yet
 
 	//set URLs an PATHs
 	$windower_links = [
